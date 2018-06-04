cbc4eccd8eda14db08de220bad65fe56f5d3dd52
Brandon Schaen <brandonlschaen@gmail.com>
2018-06-04 18:24:08 -0400

    add to task list

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("#pageForm");
 const task = document.querySelector("#taskInput");
 const taskBtn = document.querySelector("#addBtn");
-
+const list = document.querySelector("#list");
 
 
 form.addEventListener('click', clickHandler);
@@ -11,6 +11,10 @@ function clickHandler(e) {
   if(e.target.id == "addBtn"){
     loadTask = task.value;
     console.log(loadTask);
+    let li = document.createElement("li");
+    li.id = 'list';
+    li.innerHTML = `${loadTask} <a align='right' href='#'>&#10007;</a>`;
+    list.appendChild(li);
     task.value = "";
   }
   
