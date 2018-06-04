8a43b154d11d3613d6ab6f2902180b80d4c95ef8
Brandon Schaen <brandonlschaen@gmail.com>
2018-06-04 17:32:14 -0400

    get input and clear box

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const form = document.querySelector("#pageForm");
 const task = document.querySelector("#taskInput");
 const taskBtn = document.querySelector("#addBtn");
 
-console.log(taskBtn);
+
 
 form.addEventListener('click', clickHandler);
 
 function clickHandler(e) {
-
+  let loadTask
+  if(e.target.id == "addBtn"){
+    loadTask = task.value;
+    console.log(loadTask);
+    task.value = "";
+  }
+  
   e.preventDefault();
 }
